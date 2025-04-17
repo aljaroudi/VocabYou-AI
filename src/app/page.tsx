6afc67354bb76b55cfdc0afc7c5d921e30c52888
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-17 16:40:07 +0300

    refactor: improve layout and styling in Home component for better spacing and readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,21 @@ import { useLocalState } from '~/hooks/useLocalState'
 import type { Phrase } from '~/lib/types'
 
 export default function Home() {
-  const [phrases, setPhrases] = useLocalState<Phrase[]>('phrases', [])
+	const [phrases, setPhrases] = useLocalState<Phrase[]>('phrases', [])
 
-  return (
-    <main className="flex h-dvh flex-col items-center overflow-auto bg-stone-100 p-2">
-      <div className="flex w-full max-w-[40ch] flex-col gap-4">
-        <AddPhrase onAdd={p => setPhrases(prev => [p, ...prev])} />
-        {phrases.map((phrase, index) => (
-          <PhraseDef
-            key={index}
-            def={phrase}
-            onDelete={() => setPhrases(prev => prev.filter((_, i) => i !== index))}
-            onMoveUp={() => setPhrases(prev => [phrase, ...prev.filter((_, i) => i !== index)])}
-          />
-        ))}
-      </div>
-    </main>
-  )
+	return (
+		<main className="flex h-dvh flex-col items-center overflow-auto bg-stone-100 p-2">
+			<div className="flex w-full max-w-[40ch] flex-col gap-4 pb-8">
+				<AddPhrase onAdd={p => setPhrases(prev => [p, ...prev])} />
+				{phrases.map((phrase, index) => (
+					<PhraseDef
+						key={index}
+						def={phrase}
+						onDelete={() => setPhrases(prev => prev.filter((_, i) => i !== index))}
+						onMoveUp={() => setPhrases(prev => [phrase, ...prev.filter((_, i) => i !== index)])}
+					/>
+				))}
+			</div>
+		</main>
+	)
 }
