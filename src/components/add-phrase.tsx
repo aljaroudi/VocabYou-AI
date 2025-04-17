716ec88e03adbf02e6065bdf3745c3db90ad48b5
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-17 15:13:01 +0300

    feat: add language selection functionality to AddPhrase component for improved user experience

--- src/components/add-phrase.tsx
+++ src/components/add-phrase.tsx
@@ -4,11 +4,16 @@ import { useState } from 'react'
 import { Input } from './ui/input'
 import { Settings } from './settings'
 import { useLocalState } from '~/hooks/useLocalState'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
+import { LANGS } from '~/lib/constants'
+import { languageLabels } from '~/lib/i18n-helpers'
 
 export function AddPhrase({ onAdd }: { onAdd: (def: Phrase) => void }) {
   const [phrase, setPhrase] = useState('')
   const [apiKey, setApiKey] = useLocalState('apiKey', '')
   const [languages, setLanguages] = useLocalState('languages', ['en-US', 'es-ES'])
+  const [lang, setLang] = useState('auto detect')
+
   const add = api.ai.define.useMutation({
     onSuccess: data => {
       onAdd(data)
@@ -25,7 +30,7 @@ export function AddPhrase({ onAdd }: { onAdd: (def: Phrase) => void }) {
       className="flex gap-2"
       onSubmit={e => {
         e.preventDefault()
-        add.mutate({ phrase, apiKey, languages })
+        add.mutate({ phrase, apiKey, languages, lang })
         e.currentTarget.blur()
       }}
     >
@@ -44,6 +49,30 @@ export function AddPhrase({ onAdd }: { onAdd: (def: Phrase) => void }) {
         required
         disabled={add.isPending}
       />
+      <Select value={lang} onValueChange={setLang}>
+        <SelectTrigger
+          className="rounded-full bg-white disabled:animate-pulse"
+          disabled={add.isPending}
+        >
+          <SelectValue placeholder="Select a language">
+            {lang === 'auto detect'
+              ? 'Auto'
+              : languageLabels[lang as keyof typeof languageLabels].flag}
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="auto detect">
+            <span role="img" aria-label="Auto detect">
+              🧠 Auto
+            </span>
+          </SelectItem>
+          {LANGS.map(lang => (
+            <SelectItem key={lang} value={lang}>
+              {languageLabels[lang].flag} {languageLabels[lang].name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </form>
   )
 }
