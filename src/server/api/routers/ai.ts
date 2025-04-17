56fc9cbd3bdf21ffcf4848939e4376de1656e24f
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-17 15:50:23 +0300

    fix: add language parameter to AI mutation for enhanced translation context and update request format

--- src/server/api/routers/ai.ts
+++ src/server/api/routers/ai.ts
@@ -5,40 +5,41 @@ import { geminiConfig, GeminiRes } from '~/server/schema'
 import { GoogleGenAI } from '@google/genai/node'
 
 export const aiRouter = createTRPCRouter({
-  define: publicProcedure
-    .input(
-      z.object({
-        phrase: z.string(),
-        languages: z.array(z.string()),
-        apiKey: z.string(),
-      })
-    )
-    .output(
-      z.object({
-        phrase: z.string(),
-        timestamp: z.number(),
-        def: GeminiRes,
-      })
-    )
-    .mutation(async ({ input: { phrase, languages, apiKey } }) => {
-      const timestamp = new Date().getTime()
-      const ai = new GoogleGenAI({ apiKey })
+	define: publicProcedure
+		.input(
+			z.object({
+				phrase: z.string(),
+				languages: z.array(z.string()),
+				apiKey: z.string(),
+				lang: z.string(),
+			})
+		)
+		.output(
+			z.object({
+				phrase: z.string(),
+				timestamp: z.number(),
+				def: GeminiRes,
+			})
+		)
+		.mutation(async ({ input: { phrase, languages, apiKey, lang } }) => {
+			const timestamp = new Date().getTime()
+			const ai = new GoogleGenAI({ apiKey })
 
-      const res = await ai.models.generateContent({
-        model: 'gemini-2.0-flash-lite',
-        config: geminiConfig,
-        contents: [
-          {
-            role: 'user',
-            parts: [
-              {
-                text: `Target languages: [${languages.join(', ')}]\nPhrase: ${phrase}`,
-              },
-            ],
-          },
-        ],
-      })
-      if (!res.text) throw new Error('No response from AI')
-      return { phrase, def: JSON.parse(res.text) as GeminiRes, timestamp }
-    }),
+			const res = await ai.models.generateContent({
+				model: 'gemini-2.0-flash-lite',
+				config: geminiConfig,
+				contents: [
+					{
+						role: 'user',
+						parts: [
+							{
+								text: `Target languages: [${languages.join(', ')}]\nPhrase: ${phrase}\nLanguage: ${lang}`,
+							},
+						],
+					},
+				],
+			})
+			if (!res.text) throw new Error('No response from AI')
+			return { phrase, def: JSON.parse(res.text) as GeminiRes, timestamp }
+		}),
 })
