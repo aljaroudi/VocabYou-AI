f723f876e628acac2ddebc505523961b03d83d47
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-17 16:55:43 +0300

    refactor: enhance AI router response structure with detailed translation examples and improved language handling

--- src/server/api/routers/ai.ts
+++ src/server/api/routers/ai.ts
@@ -33,7 +33,76 @@ export const aiRouter = createTRPCRouter({
 						role: 'user',
 						parts: [
 							{
-								text: `Target languages: [${languages.join(', ')}]\nPhrase: ${phrase}\nLanguage: ${lang}`,
+								text: `Phrase: chips\nLanguage: auto detect\nTarget languages: [en-US, en-GB, ar-001, es-ES, es-MX]\n`,
+							},
+						],
+					},
+					{
+						role: 'model',
+						parts: [
+							{
+								text: `
+                {
+                  "originalText": { 
+                    "lang": "en-US"
+                  },
+                  "translations": [
+                    {
+                      "definition": "رقائق البطاطس",
+                      "target": "ar-001",
+                      "text": "رقائق",
+                      "examples": [
+                        "أحب رقائق البطاطس مع الصلصة."
+                      ],
+                      "func": "noun"
+                    },
+                    {
+                      "definition": "Small, thin pieces of fried potato.",
+                      "target": "en-GB",
+                      "text": "crisps",
+                      "examples": [
+                        "I like crisps with dip."
+                      ],
+                      "func": "noun"
+                    },
+                    {
+                      "definition": "Small, thin pieces of fried potato.",
+                      "target": "en-US",
+                      "text": "chips",
+                      "examples": [
+                        "I like chips with dip."
+                      ],
+                      "func": "noun"
+                    },
+                    {
+                      "definition": "Pequeños trozos de patata fritos y crujientes.",
+                      "target": "es-ES",
+                      "text": "patatas fritas",
+                      "examples": [
+                        "Me encantan las patatas fritas con ketchup."
+                      ],
+                      "func": "noun"
+                    },
+                    {
+                      "definition": "Pequeños trozos de patata fritos y crujientes.",
+                      "target": "es-MX",
+                      "text": "papas fritas",
+                      "examples": [
+                        "Me encantan las papas fritas con catsup."
+                      ],
+                      "func": "noun"
+                    }
+                  ]
+                }
+                `,
+							},
+						],
+					},
+					{
+						role: 'user',
+						parts: [
+							{
+								text: `Phrase: ${phrase}\nLanguage: ${lang}\nTarget languages: [${languages.join(', ')}]\n`,
 							},
 						],
 					},
