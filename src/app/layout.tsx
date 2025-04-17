51b10a0f887e55f962e79aee75dbac7f78d354ea
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-17 17:52:26 +0300

    refactor: integrate Rubik font alongside Geist in layout for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,34 @@
 import '~/styles/globals.css'
 
 import type { Metadata } from 'next'
-import { Geist } from 'next/font/google'
+import { Geist, Rubik } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 
 import { TRPCReactProvider } from '~/trpc/react'
 
 export const metadata: Metadata = {
-  title: 'VocabYou',
-  description: 'AI-powered tool for learning vocabulary in multiple languages.',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+	title: 'VocabYou',
+	description: 'AI-powered tool for learning vocabulary in multiple languages.',
+	icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
 const geist = Geist({
-  subsets: ['latin'],
-  variable: '--font-geist-sans',
+	subsets: ['latin'],
+	variable: '--font-geist-sans',
+})
+
+const rubik = Rubik({
+	subsets: ['latin'],
+	variable: '--font-rubik',
 })
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={geist.variable}>
-      <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-        <Analytics />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" className={`${geist.variable} ${rubik.variable}`}>
+			<body>
+				<TRPCReactProvider>{children}</TRPCReactProvider>
+				<Analytics />
+			</body>
+		</html>
+	)
 }
