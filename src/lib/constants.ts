25cfdaac52b89ff18e59d8dbfdba1c01d4fea7cb
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-17 14:29:12 +0300

    feat: update language support in constants and i18n helpers with additional languages and improved labels

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,15 +1,21 @@
 export const LANGS = [
-  'en-US',
-  'en-GB',
-  'fr-FR',
-  'es-ES',
-  'es-MX',
-  'de-DE',
-  'ar-SA',
-  'zh-CN',
-  'hi-IN',
-  'ja-JP',
-  'ru-RU',
+  'ar-001', // Modern Standard Arabic
+  'bn-BD', // Bengali (Bangladesh)
+  'de-DE', // German (Germany)
+  'en-GB', // English (United Kingdom)
+  'en-US', // English (United States)
+  'es-ES', // Spanish (Spain)
+  'es-MX', // Spanish (Mexico)
+  'fr-FR', // French (France)
+  'hi-IN', // Hindi (India)
+  'id-ID', // Indonesian (Indonesia)
+  'it-IT', // Italian (Italy)
+  'ja-JP', // Japanese (Japan)
+  'ko-KR', // Korean (South Korea)
+  'pt-BR', // Portuguese (Brazil)
+  'ru-RU', // Russian (Russia)
+  'zh-CN', // Chinese (Simplified, Mainland China)
+  'zh-TW', // Chinese (Traditional, Taiwan)
 ] as const satisfies string[]
 
 export const PARTS_OF_SPEECH = [
